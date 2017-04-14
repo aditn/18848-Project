683a77ccc192ed0cc54a1cb53f7011ccc57fb26b
Adit Namdev <aditn4@gmail.com>
2017-04-14 07:04:00 -0400

    updated client to interface with leds

--- server.py
+++ server.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask import request
 import json
+#import serial
+#import RPi.GPIO as GPIO
+
+#ser = serial.Serial('/dev/ttyACM0', 9600)
 
 app = Flask(__name__)
 
 @app.route("/",methods = ["POST", "GET"])
 def hello():
-    #request.method == 'POST'
-    print "Got: ", request.form["foo"]
-    return 'lol'
-    return json.dumps(request.form["foo"])
+  if request.method == 'POST':
+    for i in request.form:
+      command = i + ":" + request.form[i]
+      print command
+      #ser.write(command) # f is used to collect force sensor data
+
+    return "Recieved info" 
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000)
